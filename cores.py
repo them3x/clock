cf969840848d796bb93163cbf34ce3d3f975fff0
Messias Eric <67919910+them3x@users.noreply.github.com>
2021-11-26 08:05:29 -0300

    Create cores.py

---
+++ cores.py
@@ -0,0 +1,24 @@
+#encoding: UTF-8
+
+class cores:
+
+        def __init__(self):
+                self.vermelho = '\033[31m'
+                self.verde = '\033[32m'
+                self.azul = '\033[34m'
+                self.ciano = '\033[36m'
+                self.magenta = '\033[35m'
+                self.amarelo = '\033[33m'
+                self.preto = '\033[30m'
+                self.branco = '\033[37m'
+                self.f_preto = '\033[40m'
+                self.f_vermelho = '\033[41m'
+                self.f_verde = '\033[42m'
+                self.f_amarelo = '\033[43m'
+                self.f_azul = '\033[44m'
+                self.f_magenta = '\033[45m'
+                self.f_ciano = '\033[46m'
+                self.f_branco = '\033[47m'
+                self.negrito = '\033[1m'
+                self.reverso = '\033[2m'
+                self.finaliza = '\033[0;0m'
