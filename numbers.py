57ef2eed0480af3d74a7c08785cd61354670f0f5
Messias Eric <67919910+them3x@users.noreply.github.com>
2021-11-26 08:06:28 -0300

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,128 @@
+class zero():
+	def __init__ (self):
+		self.n1 = '  ########  '
+		self.n2 = ' ########## '
+		self.n3 = ' ##      ## '
+		self.n4 = ' ##      ## '
+		self.n5 = ' ##      ## '
+		self.n6 = ' ##      ## '
+		self.n7 = ' ##      ## '
+		self.n8 = ' ########## '
+		self.n9 = '  ########  '
+
+class one():
+	def __init__ (self):
+
+		self.n1 = '     ##     '
+		self.n2 = '    ##      '
+		self.n3 = '     ##     '
+		self.n4 = '     ##     '
+		self.n5 = '     ##     '
+		self.n6 = '     ##     '
+		self.n7 = '     ##     '
+		self.n8 = ' #########  '
+		self.n9 = '  #######   '
+
+class two():
+	def __init__ (self):
+
+		self.n1 = '   #######  '
+		self.n2 = '  ##    ##  '
+		self.n3 = '  ##   ##   '
+		self.n4 = '      ##    '
+		self.n5 = '     ##     '
+		self.n6 = '    ##      '
+		self.n7 = '   ##       '
+		self.n8 = '  ########  '
+		self.n9 = ' ########## '
+
+class thr():
+	def __init__ (self):
+
+		self.n1 = ' #########  '
+		self.n2 = ' ########## '
+		self.n3 = '         ## '
+		self.n4 = '         ## '
+		self.n5 = '     #####  '
+		self.n6 = '         ## '
+		self.n7 = '         ## '
+		self.n8 = ' ########## '
+		self.n9 = ' #########  '
+
+class fou():
+	def __init__ (self):
+
+		self.n1 = ' ##      ## '
+		self.n2 = ' ##      ## '
+		self.n3 = ' ##      ## '
+		self.n4 = ' ########## '
+		self.n5 = ' ########## '
+		self.n6 = '         ## '
+		self.n7 = '         ## '
+		self.n8 = '         ## '
+		self.n9 = '         ## '
+
+class fiv():
+	def __init__ (self):
+
+		self.n1 = ' ########## '
+		self.n2 = ' ########## '
+		self.n3 = ' ##         '
+		self.n4 = ' ########## '
+		self.n5 = ' ########## '
+		self.n6 = '         ## '
+		self.n7 = '         ## '
+		self.n8 = ' ########## '
+		self.n9 = ' ########## '
+
+class six():
+	def __init__ (self):
+
+		self.n1 = ' ########## '
+		self.n2 = ' ########## '
+		self.n3 = ' ##         '
+		self.n4 = ' ########## '
+		self.n5 = ' ########## '
+		self.n6 = ' ##      ## '
+		self.n7 = ' ##      ## '
+		self.n8 = ' ########## '
+		self.n9 = ' ########## '
+
+class sev():
+	def __init__ (self):
+
+		self.n1 = ' ########## '
+		self.n2 = ' ########## '
+		self.n3 = '        ##  '
+		self.n4 = '       ##   '
+		self.n5 = '      ##    '
+		self.n6 = '     ##     '
+		self.n7 = '    ##      '
+		self.n8 = '   ##       '
+		self.n9 = '  ##        '
+
+class eig():
+	def __init__ (self):
+
+		self.n1 = '  ########  '
+		self.n2 = ' ########## '
+		self.n3 = ' ##      ## '
+		self.n4 = ' ##      ## '
+		self.n5 = '  ########  '
+		self.n6 = ' ##      ## '
+		self.n7 = ' ##      ## '
+		self.n8 = ' ########## '
+		self.n9 = '  ########  '
+
+class nin():
+	def __init__ (self):
+
+		self.n1 = '  ########  '
+		self.n2 = ' ########## '
+		self.n3 = ' ##      ## '
+		self.n4 = ' ##      ## '
+		self.n5 = '  ########  '
+		self.n6 = '         ## '
+		self.n7 = '         ## '
+		self.n8 = ' ########## '
+		self.n9 = ' #########  '
