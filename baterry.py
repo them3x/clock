3aef24282940044363639e73b2b426decc94c080
Messias Eric <67919910+them3x@users.noreply.github.com>
2021-11-26 08:04:50 -0300

    Create baterry.py

---
+++ baterry.py
@@ -0,0 +1,75 @@
+class pc():
+        def __init__ (self):
+		self.n1 = " ////////////////////"
+		self.n2 = "|      ____ plugged | "
+		self.n3 = "| ____|    \     PC | "
+		self.n4 = "|(____|     `._____ | "
+		self.n5 = "| ____|       _|___ | "
+		self.n6 = "|(____|     .'      | "
+		self.n7 = "|     |____/        | "
+		self.n8 = "|                   | "
+		self.n9 = "////////////////////"
+class full():
+	def __init__ (self):
+
+		self.n1 = '     ##  '
+		self.n2 = ' ########## '
+		self.n3 = ' ########## '
+		self.n4 = ' ########## '
+		self.n5 = ' ########## '
+		self.n6 = ' ########## '
+		self.n7 = ' ########## '
+		self.n8 = ' ########## '
+		self.n9 = ' ########## '
+
+class sogood():
+	def __init__ (self):
+
+		self.n1 = '     ##  '
+		self.n2 = ' ########## '
+		self.n3 = ' #     #### '
+		self.n4 = ' ########## '
+		self.n5 = ' ########## '
+		self.n6 = ' ########## '
+		self.n7 = ' ########## '
+		self.n8 = ' ########## '
+		self.n9 = ' ########## '
+
+class good():
+	def __init__ (self):
+
+		self.n1 = '     ##  '
+		self.n2 = ' ########## '
+		self.n3 = ' #        # '
+		self.n4 = ' #      ### '
+		self.n5 = ' # ######## '
+		self.n6 = ' ########## '
+		self.n7 = ' ########## '
+		self.n8 = ' ########## '
+		self.n9 = ' ########## '
+
+class low():
+	def __init__ (self):
+
+		self.n1 = '     ##  '
+		self.n2 = ' ########## '
+		self.n3 = ' #        # '
+		self.n4 = ' #        # '
+		self.n5 = ' #        # '
+		self.n6 = ' #        # '
+		self.n7 = ' ########## '
+		self.n8 = ' ########## '
+		self.n9 = ' ########## '
+
+class solow():
+	def __init__ (self):
+
+		self.n1 = '     ##  '
+		self.n2 = ' ########## '
+		self.n3 = ' #        # '
+		self.n4 = ' #        # '
+		self.n5 = ' #        # '
+		self.n6 = ' #        # '
+		self.n7 = ' #        # '
+		self.n8 = ' #     #### '
+		self.n9 = ' ########## '
